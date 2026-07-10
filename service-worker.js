9b1d2a084b0e6b2adc0442be9b8bc1570282fd68
rajaiahboomi-crypto <rajaiahboomi@gmail.com>
2026-07-10 18:59:29 +0530

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const C='wrd-v4';const A=['./','./index.html','./manifest.webmanifest'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
